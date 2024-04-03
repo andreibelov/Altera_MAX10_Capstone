d1d9abfe22224ebcbc6253972bd59fbf1cdf3762
Andrei Belov <abelov@student.42london.com>
2024-04-03 21:54:29 +0100

    C4M1P2 got 100

--- C4M1P2/C4M1P2.vhd
+++ C4M1P2/C4M1P2.vhd
@@ -21,53 +21,53 @@ entity C4M1P2 is port (
 	-- Input ports
 	SW		: in std_logic_vector(3 downto 0);
 	-- Output ports
-	HEX0	: out std_logic_vector(6 downto 0);
-	HEX1	: out std_logic_vector(6 downto 0));
+	HEX0	: out std_logic_vector(7 downto 0);
+	HEX1	: out std_logic_vector(7 downto 0));
 end entity C4M1P2;
 
 -- Architecture of the entity
 architecture arch of C4M1P2 is
 	-- Commonly declared signals
-	signal z	: std_logic;
-	signal A	: std_logic_vector(3 downto 0);
-	signal V	: std_logic_vector(3 downto 0);
-	signal D0	: std_logic_vector(3 downto 0);
-	signal D1	: std_logic_vector(3 downto 0) := "0000";
-	type lut_array is array (0 to 15) of std_logic_vector(6 downto 0);
-	constant lut_data : lut_array := (
-			"1000000", -- 0b0000
-			"1111001", -- 0b0001
-			"0100100", -- 0b0010
-			"0110000", -- 0b0011
-			"0011001", -- 0b0100
-			"0010010", -- 0b0101
-			"0000010", -- 0b0110
-			"1111000", -- 0b0111
-			"0000000", -- 0b1000
-			"0011000", -- 0b1001
-			"0001000", -- 0b1010
-			"0000011", -- 0b1011
-			"1000110", -- 0b1100
-			"0100001", -- 0b1101
-			"0000110", -- 0b1110
-			"0001110" -- 0b1111
-		);
+	signal z		: std_logic;
+	signal A		: std_logic_vector(3 downto 0);
+	signal V		: std_logic_vector(3 downto 0);
+	signal oHEX0	: std_logic_vector(7 downto 0);
+	signal oHEX1	: std_logic_vector(7 downto 0);
+	signal iDIG0	: std_logic_vector(3 downto 0);
+	signal iDIG1	: std_logic_vector(3 downto 0) := "0000";
 begin
 
 	-- Concurrent Signal Assignment
 	V <= SW(3 downto 0);
 	z <= (V(3) and V(2)) or (V(3) and V(1));
 
-	D1(0) <= z;
+	iDIG1(0) <= z;
 
-	A <= std_logic_vector(to_unsigned(to_integer(unsigned(V)) mod 10, 4));
+	A(3) <= '0';
+	A(2) <= SW(3) and SW(2) and SW(1);
+	A(1) <= SW(3) and SW(2) and not(SW(1));
+	A(0) <= (SW(3) and SW(2) and SW(0)) or (SW(3) and SW(1) and SW(0));
 
-	D0(0) <= (V(0) and (not z)) or (A(0) and z);
-	D0(1) <= (V(1) and (not z)) or (A(1) and z);
-	D0(2) <= (V(2) and (not z)) or (A(2) and z);
-	D0(3) <= (V(3) and (not z)) or (A(3) and z);
+	-- Selected Signal Assignment
+	-- The selected signal assignment (with <expr> select ...) is essentially
+	-- a short-hand notion for creating a process along with the appropriate
+	-- sensitivity list and case statement.
+	with z select
+		iDIG0 <= V when '0',
+				 A when '1',
+			"0000" when others;
 
-	HEX0 <= lut_data(to_integer(unsigned(D0)));
-	HEX1 <= lut_data(to_integer(unsigned(D1)));
+	u0 : work.SEG7_LUT
+		port map (
+			iDIG => iDIG0,
+			oSEG => oHEX0);
+
+	u1 : work.SEG7_LUT
+		port map (
+			iDIG => iDIG1,
+			oSEG => oHEX1);
+
+	HEX0 <= oHEX0;
+	HEX1 <= oHEX1;
 
 end architecture arch;
